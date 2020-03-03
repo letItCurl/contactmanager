e9b31aa1371bf747ec4fda4f2a736970e6868ba5
rollpz <rolandlopez.developer@gmail.com>
2020-03-03 13:50:10 +0100

    Woking on pics

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :group
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://picsum.photos/seed/#{hash}/100/100"
+  end
+
 end
