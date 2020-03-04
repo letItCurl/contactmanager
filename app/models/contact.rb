56863e3e719f28c04c41d7749c7ffe81fad3a25a
rollpz <rolandlopez.developer@gmail.com>
2020-03-04 13:17:11 +0100

    add file / update model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,8 @@ class Contact < ApplicationRecord
   validates :name, :email, :group_id, presence: true
   validates :name, length: {minimum: 2}
 
+  include ImageUploader::Attachment(:image) # adds an `image` virtual attribute
+  
   def gravatar
     hash = Digest::MD5.hexdigest(email.downcase)
     "https://picsum.photos/seed/#{hash}/100/100"
