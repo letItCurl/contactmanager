ed8d3508eeafba6f43db872cfd8128c0e407e453
rollpz <rolandlopez.developer@gmail.com>
2020-04-13 19:33:54 +0200

    customizing pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
     include Pundit
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
     protected
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || dashboard_index_path
@@ -11,5 +14,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+    def user_not_authorized
+        flash[:danger] = "What a naughty person you are!!!"
+        redirect_to action: :index
     end
 end
